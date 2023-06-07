fe7b147c51794f35b968300dd8b8a4f89c1b3370
Bluebee45 <Santi72143@gmail.com>
2023-06-07 15:31:50 -0500

    add file

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -2,5 +2,5 @@
 def square_matrix_simple(matrix=[]):
     new_list = list()
     for i in range(0, len(matrix)):
-         new_list.append(list(map(lambda x: x**2, matrix[i])))
+        new_list.append(list(map(lambda x: x**2, matrix[i])))
     return new_list
