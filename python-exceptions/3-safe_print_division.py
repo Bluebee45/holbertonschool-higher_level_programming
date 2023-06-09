ffc87789a749ddee9c3bfabc503efd81b55c7b95
Bluebee45 <santi72143@gmail.com>
2023-06-09 17:43:45 -0500

    add file

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        division = a / b
+        print("Inside result: {:.1f}".format(division))
+    except (ZeroDivisionError, TypeError):
+        division = None
+        print("Inside result: {}".format(division))
+    finally:
+        return division
