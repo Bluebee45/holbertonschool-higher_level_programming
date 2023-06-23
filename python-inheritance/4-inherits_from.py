3254bee9bbc1f640fc5457ba025b92c475887a15
Bluebee45 <santi72143@gmail.com>
2023-06-22 19:16:20 -0500

    add file

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """
-4.Only sub class of
+4. Only sub class of
 Function that returns True if the object is an instance of a class that
-inherited from the specified class
+inherited from the specified class ; otherwise False
 """
+
 
 def inherits_from(obj, a_class):
     """
-    Function that returns True or False
+    Funtion that returns True or False
     """
     if isinstance(obj, a_class):
         if type(obj) is not a_class:
